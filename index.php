173cf7e9c8c7f9920184845096373a993ad92cc9
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-08 12:48:04 +0200

    Added navbar
    Updated layout and added sticky navbar.

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 include_once './header.php';
 ?>
 
-    <h1>Title of front page</h1>
-    <p>Lorem ipsum....................................</p>
-
+<div class="container my-5 text-center">
+    <div class="row">
+        <div class="col">
+            <h1>Title of front page</h1>
+            <p>Lorem ipsum....................................</p>
+        </div>
+    </div>
+</div>
 
 <?php
 include_once './footer.php';
