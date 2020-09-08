fc2c0ba49b64e911165efffd4a37667da63cb1dc
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-08 12:16:45 +0200

    Updated the basic layout of the page.
    Added the header and footer + included bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reddit-clone</title>
-</head>
-<body>
+<?php
+include_once './header.php';
+?>
+
     <h1>Title of front page</h1>
     <p>Lorem ipsum....................................</p>
-</body>
-</html>
+
+
+<?php
+include_once './footer.php';
+?>
