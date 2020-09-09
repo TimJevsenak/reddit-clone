9d5469951163e5b0f901aef693d28eed19655f6a
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-09 09:06:37 +0200

    Added site icons

--- header.php
+++ header.php
@@ -5,18 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Reddit-clone</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="icon" href="siteIcons/reddit.png" type="png" sizes="16x16">
     <script src="https://kit.fontawesome.com/9546f008dc.js" crossorigin="anonymous"></script>
     <script src="icons.js" defer></script>
 </head>
 <body>
     <nav class="navbar navbar-expand-md navbar-dark sticky-top bg-dark">
-  <a class="navbar-brand" href="index.php">Reddit-clone</a>
+  <a class="navbar-brand text-lowercase" href="index.php"><img src="siteIcons/reddit.png" height="32px" width="32px"> Reddit-clone</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <form class="form-inline mt-2 mt-md-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <input id="search" class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
     <ul class="navbar-nav ml-auto">
