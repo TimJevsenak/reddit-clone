07cc6464c01a54d1a078cb5a9acde3d235fd104c
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-09 09:43:47 +0200

    Get user_id when logging in

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ include_once './header.php';
         <div class="col">
             <h1>Title of front page<i class="fad fa-bookmark"></i></h1>
             <p>Lorem ipsum....................................</p>
-            <?php if(isset($_SESSION['user_id']))echo $_SESSION['user_id']; ?>
+            <?php if(isset($_SESSION['user_id']))
+                    echo $_SESSION['user_id']; 
+            ?>
         </div>
     </div>
 </div> 
