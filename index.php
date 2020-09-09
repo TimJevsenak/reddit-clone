36ff45d66e20c18ae42fceeff90b29447c9f2cde
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-09 09:40:11 +0200

    Added login
    Added the option of logging in

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include_once './header.php';
         <div class="col">
             <h1>Title of front page<i class="fad fa-bookmark"></i></h1>
             <p>Lorem ipsum....................................</p>
+            <?php if(isset($_SESSION['user_id']))echo $_SESSION['user_id']; ?>
         </div>
     </div>
 </div> 
--- login.php
+++ login.php
@@ -5,12 +5,12 @@ include_once './header.php';
     <div class="row">
         <div class="col-4"></div>
         <div class="col-4 my-5">
-            <form class="form-signin">
+            <form action="login_check.php" method="post" class="form-signin">
                 <h1 class="h2 my-4 font-weight-normal text-center">Sign In</h1>
                 <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" class="form-control my-1" placeholder="Email address" required="" autofocus="">
+                <input name="email" type="email" id="inputEmail" class="form-control my-1" placeholder="Email address" required="" autofocus="">
                 <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" class="form-control my-1" placeholder="Password" required="">
+                <input name="password" type="password" id="inputPassword" class="form-control my-1" placeholder="Password" required="">
                 <div class="checkbox my-3 text-center">
                 <label>
                 <input type="checkbox" value="remember-me"> Remember me
