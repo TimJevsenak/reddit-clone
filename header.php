52c0ffb3bc0547b3cd550667b75f39a68cf0c944
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-22 12:00:00 +0200

    new temporary logo

--- header.php
+++ header.php
@@ -8,13 +8,13 @@ include_once './session.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Reddit-clone</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="icon" href="siteIcons/reddit.png" type="png" sizes="16x16">
+    <link rel="icon" href="siteIcons/logo.jpg" type="png" sizes="16x16">
     <script src="https://kit.fontawesome.com/9546f008dc.js" crossorigin="anonymous"></script>
     <script src="icons.js" defer></script>
 </head>
 <body>
     <nav class="navbar navbar-expand-md navbar-dark sticky-top" style="background-color: #232323;">
-  <a class="navbar-brand text-lowercase" href="index.php"><img src="siteIcons/reddit.png" height="32px" width="32px"> Reddit-clone</a>
+  <a class="navbar-brand text-lowercase" href="index.php"><img src="siteIcons/logo.jpg" height="32px" width="32px"> Read-It</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
