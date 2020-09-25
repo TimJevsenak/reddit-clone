a1824ff97e95eca55d8aa9cae90c95a1b7d43699
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-25 09:09:39 +0200

    Changed page title

--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@ include_once './database.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Reddit-clone</title>
+    <title>read-it</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="siteIcons/logo.jpg" type="png" sizes="16x16">
