619df0f7aa67557593c31ce4a3f0ebff0bed3dda
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-28 19:36:27 +0200

    DateCreated added to communities list

--- communities.php
+++ communities.php
@@ -8,11 +8,13 @@
     $stmt = $pdo->prepare($query);
     $stmt->execute();
 
-    echo '<div class="container mt-5"><div class="row"><div class="col-3"></div><div class="col-6 text-center">';
+    echo '<div class="container mt-5"><div class="row"><div class="col-3"></div><div class="col-6">';
     if($stmt->rowCount() > 0) {
         while($st < $stmt->rowCount()) {
             $community = $stmt->fetch();
-            echo '<img src="community-uploads/' . $community['id'] . "/" . $community['icon'] . '" class="img-fluid img-thumbnail" width="64" height="64"><a href="community_show.php?id='. $community['id'] .'" class=""><h5 class="">' . $community['name'] . '</h5></a><p class="text-muted mb-5">' . $community['title'] . '</p>';
+            $date = new \DateTime($community['dateCreated']);
+            $datum = $date->format('d/m/Y');
+            echo '<img src="community-uploads/' . $community['id'] . "/" . $community['icon'] . '" class="img-fluid img-thumbnail" width="64" height="64"><a href="community_show.php?id='. $community['id'] .'" class=""><h5 class="">' . $community['name'] . '</h5></a><p class="text-muted mb-0">' . $community['title'] . '</p><div class="text-right mb-5"><p class="text-muted">' . $datum . '</p></div>';
             $st++;
         }
     } else {
