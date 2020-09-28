1c7804eb4b6eaf922d7a1cccf2d17140ded75cf6
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-28 19:18:24 +0200

    Added a list of all communities to the front page

--- index.php
+++ index.php
@@ -6,11 +6,10 @@ include_once './header.php';
 
   <section class="jumbotron text-center">
     <div class="container">
-      <h1>Album example</h1>
-      <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+      <h1>Home</h1>
+      <p class="lead text-muted">Read-it is a reddit clone I'm makeing for a school project. <br>It's goal is to have the basic functions of reddit.</p>
       <p>
-        <a href="#" class="btn btn-primary my-2">Main call to action</a>
-        <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+        <a href="communities.php" class="btn btn-primary my-2">Explore Communities</a>
       </p>
     </div>
   </section>
