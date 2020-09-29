df6dd3107da8cbb866d6b6fc488a0de0ba2415f1
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-29 21:02:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ function time_elapsed_string($datetime, $full = false) {
 $st = 0;
 
 if(isset($_SESSION['user_id'])){
-$query = "SELECT DISTINCT p.id, p.title, p.post, p.image, p.date, c.name, c.icon, u.username
+$query = "SELECT DISTINCT p.id, p.title, p.post, p.image, p.date, p.community_id, c.name, c.icon, u.username
  FROM posts p INNER JOIN communities c ON p.community_id=c.id INNER JOIN subscriptions s ON s.community_id=c.id INNER JOIN users u ON p.user_id=u.id
  WHERE s.user_id=?
  ORDER BY p.date";
@@ -68,30 +68,43 @@ $stmt->execute([$_SESSION['user_id']]);
                 $post = $stmt->fetch();
                 $date = time_elapsed_string($post['date']);
                 echo '<div class="col-2"></div>
-              <div class="col-lg-8">
-                <div class="card mb-4 shadow-sm">
-                <h5 class="card-title text-center my-2">' . $post['title'] . '</h5>
-                  <img src="post-uploads/' . $post['id'] . '/' . $post['image'] .'" class="img-fluid" width="100%" height="100%">
-                  <div class="card-body">
-                    <p class="card-text">' . $post['post'] . '</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <i class="far fa-arrow-square-up fa-2x mx-1"></i><span class="font-weight-bold">0</span> 
-                        <i class="far fa-arrow-square-down mx-1 fa-2x"></i><span class="font-weight-bold">0</span>
-                      </div>
-                      <small class="text-muted">' . $date . '</small>
+            <div class="col-lg-8">
+              <div class="card mb-4 shadow-sm">
+                <div class="card-title my-2">
+                  <div class="row">
+                    <div class="col-3 text-left px-4">
+                    <img src="community-uploads/' . $post['community_id'] . '/' . $post['icon'] .'" class="img-fluid" width="32" height="32" style="border-radius: 50%;">
+                      <span class="text-muted">r/</span>' . $post['name'] . '
+                    </div>
+                    <div class="col-6 text-center">
+                      ' . $post['title'] . '
+                    </div>
+                    <div class="col-3 text-right px-4">
+                    By <span class="text-muted"> u/</span>' . $post['username'] . '
                     </div>
                   </div>
                 </div>
+                <img src="post-uploads/' . $post['id'] . '/' . $post['image'] .'" class="img-fluid" width="100%" height="100%">
+                <div class="card-body">
+                  <p class="card-text">' . $post['post'] . '</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <i class="far fa-arrow-square-up fa-2x mx-1"></i><span class="font-weight-bold">0</span> 
+                      <i class="far fa-arrow-square-down mx-1 fa-2x"></i><span class="font-weight-bold">0</span>
+                    </div>
+                    <small class="text-muted">' . $date . '</small>
+                  </div>
+                </div>
               </div>
-              <div class="col-2"></div>';
+            </div>
+            <div class="col-2"></div>';
                 $st++;
             }
           } 
           else {
             $st = 0;
 
-            $query = "SELECT DISTINCT p.id, p.title, p.post, p.image, p.date, c.name, c.icon, u.username
+            $query = "SELECT DISTINCT p.id, p.title, p.post, p.image, p.date, p.community_id, c.name, c.icon, u.username
             FROM posts p INNER JOIN communities c ON p.community_id=c.id INNER JOIN subscriptions s ON s.community_id=c.id INNER JOIN users u ON p.user_id=u.id
             ORDER BY p.date";
             $stmt = $pdo->prepare($query);
@@ -101,23 +114,36 @@ $stmt->execute([$_SESSION['user_id']]);
                 $post = $stmt->fetch();
                 $date = time_elapsed_string($post['date']);
                 echo '<div class="col-2"></div>
-              <div class="col-lg-8">
-                <div class="card mb-4 shadow-sm">
-                <h5 class="card-title text-center my-2">' . $post['title'] . '</h5>
-                  <img src="post-uploads/' . $post['id'] . '/' . $post['image'] .'" class="img-fluid" width="100%" height="100%">
-                  <div class="card-body">
-                    <p class="card-text">' . $post['post'] . '</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <i class="far fa-arrow-square-up fa-2x mx-1"></i><span class="font-weight-bold">0</span> 
-                        <i class="far fa-arrow-square-down mx-1 fa-2x"></i><span class="font-weight-bold">0</span>
-                      </div>
-                      <small class="text-muted">' . $date . '</small>
+            <div class="col-lg-8">
+              <div class="card mb-4 shadow-sm">
+                <div class="card-title my-2">
+                  <div class="row">
+                    <div class="col-3 text-left px-4">
+                    <img src="community-uploads/' . $post['community_id'] . '/' . $post['icon'] .'" class="img-fluid" width="32" height="32" style="border-radius: 50%;">
+                      <span class="text-muted">r/</span>' . $post['name'] . '
+                    </div>
+                    <div class="col-6 text-center">
+                      ' . $post['title'] . '
+                    </div>
+                    <div class="col-3 text-right px-4">
+                    By <span class="text-muted"> u/</span>' . $post['username'] . '
                     </div>
                   </div>
                 </div>
+                <img src="post-uploads/' . $post['id'] . '/' . $post['image'] .'" class="img-fluid" width="100%" height="100%">
+                <div class="card-body">
+                  <p class="card-text">' . $post['post'] . '</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <i class="far fa-arrow-square-up fa-2x mx-1"></i><span class="font-weight-bold">0</span> 
+                      <i class="far fa-arrow-square-down mx-1 fa-2x"></i><span class="font-weight-bold">0</span>
+                    </div>
+                    <small class="text-muted">' . $date . '</small>
+                  </div>
+                </div>
               </div>
-              <div class="col-2"></div>';
+            </div>
+            <div class="col-2"></div>';
                 $st++;
               }
             }
