bd0d5e91c71132b99e55ecba911e9581a91e1d2e
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-29 19:15:18 +0200

    Basic look of front page posts

--- index.php
+++ index.php
@@ -4,36 +4,36 @@ include_once './header.php';
 
 <main role="main">
 
-  <section class="jumbotron text-center">
-    <div class="container">
+    <div class="container text-center mt-5">
+      <h3><i class="fas fa-home-lg-alt"></i></h3>
       <h1>Home</h1>
       <p class="lead text-muted">Read-it is a reddit clone I'm makeing for a school project. <br>It's goal is to have the basic functions of reddit.</p>
       <p>
         <a href="communities.php" class="btn btn-primary my-2">Explore Communities</a>
       </p>
     </div>
-  </section>
 
   <div class="album py-5 bg-light">
     <div class="container">
 
-      <div class="row">
-        <div class="col-md-4">
+      <div class="row mt-5">
+        <div class="col-lg-6">
           <div class="card mb-4 shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <h5 class="card-title text-center my-2">Card title</h5>
+            <img src="post-uploads/2/Man-Silhouette.jpg" class="img-fluid" width="100%" height="100%">
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  <i class="far fa-arrow-square-up fa-2x mx-1"></i><span class="font-weight-bold">0</span> 
+                  <i class="far fa-arrow-square-down mx-1 fa-2x"></i><span class="font-weight-bold">0</span>
                 </div>
                 <small class="text-muted">9 mins</small>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-lg-6">
           <div class="card mb-4 shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
             <div class="card-body">
