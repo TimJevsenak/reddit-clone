dd7855737707b2785c88292d6d38cba672ced05a
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-09-30 08:53:24 +0200

    Update community_show.php
    community_show.php now displays the posts posted to the community.

--- community_show.php
+++ community_show.php
@@ -31,8 +31,46 @@
         $join = 'JOINED';
     }
     else{
+        $disabled = '';
         $join = 'JOIN';
     }
+    function time_elapsed_string($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+      
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+      
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+      
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+      }
+      
+      $st = 0;
+      
+      $query = "SELECT DISTINCT p.id, p.title, p.post, p.image, p.date, p.community_id, c.name, c.icon, u.username
+       FROM posts p INNER JOIN communities c ON p.community_id=c.id INNER JOIN subscriptions s ON s.community_id=c.id INNER JOIN users u ON p.user_id=u.id
+       WHERE s.community_id=?
+       ORDER BY p.date";
+      $stmt = $pdo->prepare($query);
+      $stmt->execute([$id]);
 ?>
 
 <div class="container mt-5">
@@ -50,6 +88,51 @@
             <h2 class="my-2"><?php echo $community['name']; ?></h2>
             <h5 class="mt-5 mb-2"><?php echo $community['title']; ?></h5>
             <p class="my-3"><?php echo $community['description'] ?></p>
+            <h3 class="my-5">Posts</h3>
+            <?php 
+                if($stmt->rowCount() > 0) {
+                    while($st < $stmt->rowCount()) {
+                        $post = $stmt->fetch();
+                        $date = time_elapsed_string($post['date']);
+                        echo '
+                    <div class="mt-5">
+                      <div class="card mb-4 shadow-sm">
+                        <div class="card-title my-2">
+                          <div class="row">
+                            <div class="col-3 text-left px-4">
+                            <a href="community_show.php?id=' . $post['community_id'] . '"><img src="community-uploads/' . $post['community_id'] . '/' . $post['icon'] .'" class="img-fluid" width="32" height="32" style="border-radius: 50%;">
+                              <span class="text-muted">r/</span>' . $post['name'] . '</a>
+                            </div>
+                            <div class="col-6 text-center">
+                              ' . $post['title'] . '
+                            </div>
+                            <div class="col-3 text-right px-4">
+                            By <span class="text-muted"> u/</span>' . $post['username'] . '
+                            </div>
+                          </div>
+                        </div>
+                        <img src="post-uploads/' . $post['id'] . '/' . $post['image'] .'" class="img-fluid" width="100%" height="100%">
+                        <div class="card-body">
+                          <p class="card-text">' . $post['post'] . '</p>
+                          <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                              <i class="far fa-arrow-square-up fa-2x mx-1"></i><span class="font-weight-bold">0</span> 
+                              <i class="far fa-arrow-square-down mx-1 fa-2x"></i><span class="font-weight-bold">0</span>
+                            </div>
+                            <small class="text-muted">' . $date . '</small>
+                          </div>
+                        </div>
+                      </div>
+                    </div>';
+                        $st++;
+                    }
+                  } 
+                  else {
+                        echo '<div class="alert alert-info mt-5" role="alert">
+                        This community has no posts yet.
+                        </div>';
+                    }
+            ?>
         </div>
         <div class="col-2">
             <?php
