636b2070edc5cc6e7ad9a1f366e429bfd7b5ab0c
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-10-02 09:10:14 +0200

    Update user_insert.php
    Can't have users with same email.

--- user_insert.php
+++ user_insert.php
@@ -6,38 +6,48 @@ $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $password_confirm = $_POST['password_confirm'];
+
+$query = "SELECT * FROM users WHERE email=?";
+$stmt = $pdo->prepare($query);
+$stmt->execute([$email]);
+
+if($stmt->rowCount() == 0){
 //preverim podatke, da so obvezi vnešeni
-if (!empty($username)
-        && !empty($email) && !empty($password)
-        && ($password == $password_confirm)) {
-    
-    //$pass = sha1($pass1.$salt);
-    $pass = password_hash($password, PASSWORD_DEFAULT);
-    
-    $query = "INSERT INTO users (username,email,pass)"
-            . "VALUES (?,?,?)";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute([$username,$email,$pass]);
+    if (!empty($username)
+            && !empty($email) && !empty($password)
+            && ($password == $password_confirm)) {
+        
+        //$pass = sha1($pass1.$salt);
+        $pass = password_hash($password, PASSWORD_DEFAULT);
+        
+        $query = "INSERT INTO users (username,email,pass)"
+                . "VALUES (?,?,?)";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$username,$email,$pass]);
 
-    $query2 = "SELECT * FROM users WHERE email=?";
-    $stmt = $pdo->prepare($query2);
-    $stmt->execute([$email]);
+        $query2 = "SELECT * FROM users WHERE email=?";
+        $stmt = $pdo->prepare($query2);
+        $stmt->execute([$email]);
 
-    if ($stmt->rowCount() == 1) {
-        $user = $stmt->fetch();
+        if ($stmt->rowCount() == 1) {
+            $user = $stmt->fetch();
+        }
+        mkdir("user-uploads/".$user['id']);
+
+        $_SESSION['user_id'] = $user['id']; 
+        $_SESSION['username'] = $user['username']; 
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['displayname'] = $user['displayname'];
+        $_SESSION['description'] = $user['description'];
+        $_SESSION['avatar'] = $user['avatar'];
+        
+        header("Location: index.php");
     }
-    mkdir("user-uploads/".$user['id']);
-
-    $_SESSION['user_id'] = $user['id']; 
-    $_SESSION['username'] = $user['username']; 
-    $_SESSION['email'] = $user['email'];
-    $_SESSION['displayname'] = $user['displayname'];
-    $_SESSION['description'] = $user['description'];
-    $_SESSION['avatar'] = $user['avatar'];
-    
-    header("Location: index.php");
+    else {
+        header("Location: register.php");
+    }
 }
-else {
-    header("Location: registration.php");
+else{
+    header("Location: register.php");
 }
 ?>
