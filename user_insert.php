cbde7bb1358dbb204447a202567fe004f0ff76bc
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-10-04 19:53:20 +0200

    Update user_insert.php

--- user_insert.php
+++ user_insert.php
@@ -22,41 +22,58 @@ if($stmt->rowCount() == 0){
                 && !empty($email) && !empty($password)
                 && ($password == $password_confirm)) {
             
-            $pass = password_hash($password, PASSWORD_DEFAULT);
+            if(strlen($password) > 5){
             
-            $query = "INSERT INTO users (username,email,pass)"
-                    . "VALUES (?,?,?)";
-            $stmt = $pdo->prepare($query);
-            $stmt->execute([$username,$email,$pass]);
+                $pass = password_hash($password, PASSWORD_DEFAULT);
+                
+                $query = "INSERT INTO users (username,email,pass)"
+                        . "VALUES (?,?,?)";
+                $stmt = $pdo->prepare($query);
+                $stmt->execute([$username,$email,$pass]);
 
-            $query2 = "SELECT * FROM users WHERE email=?";
-            $stmt = $pdo->prepare($query2);
-            $stmt->execute([$email]);
+                $query2 = "SELECT * FROM users WHERE email=?";
+                $stmt = $pdo->prepare($query2);
+                $stmt->execute([$email]);
 
-            if ($stmt->rowCount() == 1) {
-                $user = $stmt->fetch();
+                if ($stmt->rowCount() == 1) {
+                    $user = $stmt->fetch();
+                }
+                mkdir("user-uploads/".$user['id']);
+
+                $_SESSION['user_id'] = $user['id']; 
+                $_SESSION['username'] = $user['username']; 
+                $_SESSION['email'] = $user['email'];
+                $_SESSION['displayname'] = $user['displayname'];
+                $_SESSION['description'] = $user['description'];
+                $_SESSION['avatar'] = $user['avatar'];
+                
+                echo '
+                    <script type="text/javascript">
+
+                    Swal.fire({
+                        icon: "success",
+                        text: "Account added!",
+                    }).then(function() {
+                        window.location = "index.php";
+                    });
+
+                    </script>
+                    ';
             }
-            mkdir("user-uploads/".$user['id']);
-
-            $_SESSION['user_id'] = $user['id']; 
-            $_SESSION['username'] = $user['username']; 
-            $_SESSION['email'] = $user['email'];
-            $_SESSION['displayname'] = $user['displayname'];
-            $_SESSION['description'] = $user['description'];
-            $_SESSION['avatar'] = $user['avatar'];
-            
-            echo '
+            else{
+                echo '
                 <script type="text/javascript">
 
                 Swal.fire({
-                    icon: "success",
-                    text: "Account added!",
+                    icon: "error",
+                    text: "Password to short!",
                 }).then(function() {
-                    window.location = "index.php";
+                    window.location = "register.php";
                 });
 
                 </script>
                 ';
+            }
         }
         else {
             echo '
