e593a08abc9b6a2c5bee42afea1227f409ad906c
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-10-05 19:29:02 +0200

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -3,14 +3,21 @@
     include_once 'database.php';
     include_once 'session.php';
 ?>
-<form action="forgot_password_send.php" method="post" class="form-signin">
-    <h1 class="h2 my-4 font-weight-normal text-center">Forgot password</h1>
-    <label for="inputEmail" class="sr-only">Email address</label>
-    <input name="email" type="email" id="inputEmail" class="form-control my-1" placeholder="Email address" required="" autofocus="">
-    <div class="text-center mt-5">
-        <button class="btn btn-primary" type="submit">Send mail</button>
+<div class="row">
+    <div class="col-4"></div>
+    <div class="col-lg-4">
+        <form action="forgot_password_send.php" method="post" class="form-signin">
+        <h1 class="h2 my-4 font-weight-normal text-center">Forgot password</h1>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input name="email" type="email" id="inputEmail" class="form-control my-1" placeholder="Email address" required="" autofocus="">
+        <div class="text-center mt-5">
+            <button class="btn btn-primary" type="submit">Send mail</button>
+        </div>
+        </form>
     </div>
-</form>
+    <div class="col-4"></div>
+</div>
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
