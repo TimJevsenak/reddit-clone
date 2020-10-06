6983d0ca6d2efb146f6131f485a19056b0e10769
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2020-10-06 13:03:14 +0200

    text markup

--- comment_insert.php
+++ comment_insert.php
@@ -5,8 +5,34 @@
         header('location: index.php');
     }
 
+		$version = 0.1;
+		function hyperlinks( $text = null )
+		{
+			if($text == null)
+			{
+				return false;
+			}
+			$text = htmlentities($text);
+			return preg_replace('/(ftp:\/\/|irc:\/\/|ircs:\/\/|http:\/\/|https:\/\/)([a-zA-Z0-9\/\.\-_:\?,\=]+)/i', '<a href="$1$2">$2</a>', $text);
+		}
+        function postMarkup( $text = null )
+		{
+			if( $text == null )
+			{
+				return false;
+			}
+			$text = htmlentities($text);
+			$text = preg_replace('/\[([a-zA-Z0-9\/\.\-_:\?,\=\s]+)\]\((ftp:\/\/|irc:\/\/|ircs:\/\/|http:\/\/|https:\/\/)([a-zA-Z0-9\/\.\-_:\?,\=]+)\)/i', '<a href="$2$3" target=_new>$1</a>', $text);
+			$text = preg_replace('/\*\*([a-zA-Z0-9\/\.\-_:\?,\=\s\)\(\*\&\^\!\@\#\$\%\^\+\=\`\~\{\}\[\]]+)\*\*/i', '<strong>$1</strong>', $text);
+			$text = preg_replace('/\-\-([a-zA-Z0-9\/\.\-_:\?,\=\s\)\(\*\&\^\!\@\#\$\%\^\+\=\`\~\{\}\[\]]+)\-\-/i', '<del>$1</del>', $text);
+			$text = preg_replace('/\`([a-zA-Z0-9\/\.\-_:\?,\=\s\)\(\*\&\^\!\@\#\$\%\^\+\=\`\~\{\}\[\]]+)\`/i', '<code>$1</code>', $text);
+			$text = preg_replace('/\~\~([a-zA-Z0-9\/\.\-_:\?,\=\s\)\(\*\&\^\!\@\#\$\%\^\+\=\`\~\{\}\[\]]+)\~\~/i', '<em>$1<\em>', $text);
+			$text = preg_replace('/\_\_([a-zA-Z0-9\/\.\-_:\?,\=\s\)\(\*\&\^\!\@\#\$\%\^\+\=\`\~\{\}\[\]]+)\_\_/i', '<u>$1</u>', $text);
+			return $text;
+		}
+
     $id=$_GET['id'];
-    $text = $_POST['text'];
+    $text = postMarkup($_POST['text']);
     if(empty($id) || empty($text)){
         header('location: index.php');
     }
